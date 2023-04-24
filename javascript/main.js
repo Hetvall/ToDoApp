6eb71ac87f0c16347544e212c96d29d04a2354bd
James Orozco <jamesorozcoh@gmail.com>
2023-04-24 01:23:44 -0500

    ToDoList first commit

---
+++ javascript/main.js
@@ -0,0 +1,7 @@
+
+const textToDo = document.querySelector(".text-ToDo");
+const saveButton = document.querySelector(".Save-Button");
+
+saveButton.addEventListener("click", () => {
+  console.log('estoy dando click')
+  });
