8ebd7d388cbfc494d28ba62fc40204c08792435c
James Orozco <jamesorozcoh@gmail.com>
2023-05-01 21:23:37 -0500

    comentario

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,19 @@
+// Save button.
 
-const textToDo = document.querySelector(".text-ToDo");
 const saveButton = document.querySelector(".Save-Button");
+const toDoList = document.querySelector(".toDo-List");
 
-saveButton.addEventListener("click", () => {
-  console.log('estoy dando click')
-  });
+saveButton.addEventListener("click", handleSaveToDos);
+function handleSaveToDos() {
+  const textToDo = document.querySelector(".text-ToDo").value;
+  if (textToDo) {
+    const toDo = document.createElement("li");
+    toDo.innerHTML = textToDo;
+    toDoList.appendChild(toDo);
+  } else {
+    alert("Este campo es requerido");
+  } 
+}
+
+// Notes Bloc.
+
