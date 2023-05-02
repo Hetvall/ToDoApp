982d4f68c3c227d706212433d9e15642ee82b744
kanmus7 <sebastiangc.2309@gmail.com>
2023-05-01 22:17:42 -0500

    add remove functionality for todos

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,38 @@
 // Save button.
 
+
 const saveButton = document.querySelector(".Save-Button");
 const toDoList = document.querySelector(".toDo-List");
+let textTodo = document.querySelector(".text-ToDo")
+let itemId = 0
 
 saveButton.addEventListener("click", handleSaveToDos);
-function handleSaveToDos() {
-  const textToDo = document.querySelector(".text-ToDo").value;
-  if (textToDo) {
+
+textTodo.addEventListener("keydown", (event)=> {
+  if(event.key === "Enter"){
+    event.preventDefault();
+  }
+})
+
+function handleSaveToDos(event) {
+  event.preventDefault();
+  if (textTodo.value.trim()) {
     const toDo = document.createElement("li");
-    toDo.innerHTML = textToDo;
-    toDoList.appendChild(toDo);
+    toDo.className = "item-list"
+    toDo.id = itemId++
+    toDo.innerHTML = textTodo.value;
+    toDoList.appendChild(toDo);   
+    toDo.addEventListener("click", ()=> handleDeleteItem(toDo))
   } else {
     alert("Este campo es requerido");
   } 
+  textTodo.value = ""
 }
+
+
+ const handleDeleteItem = (toDo) => {
+  toDoList.removeChild(toDo)
+ } 
 
 // Notes Bloc.
 
